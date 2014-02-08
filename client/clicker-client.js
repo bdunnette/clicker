6068d3095e5d05272ce2b85999fcc59e94fd0d85
Brian Dunnette <brian@dunnette.us>
2014-02-07 20:43:16 -0600

    cleaning up css

--- client/clicker-client.js
+++ client/clicker-client.js
@@ -1,10 +1,15 @@
-Template.poll.events = {
-  'click': function () {
-    Session.set("selected_choice", this[0]);
-  }
+Template.choice.selected = function () {
+  console.log(this);
+  return Session.equals("selected_choice", this[0]) ? "selected" : '';
 };
 
 Template.poll.selected_choice = function() {
   var selected_choice = Session.get("selected_choice");
   return selected_choice;
-}
+};
+
+Template.poll.events = {
+  'click': function () {
+    Session.set("selected_choice", this[0]);
+  }
+};
