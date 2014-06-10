9fdd2540621b7f8ed03118f62e1377587595bbb9
Brian Dunnette <dunn0172@umn.edu>
2014-06-10 15:38:14 -0500

    Update clicker-shared.js

--- clicker-shared.js
+++ clicker-shared.js
@@ -1,6 +1,3 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
-
 Polls = new Meteor.Collection("polls");
 PollChoices = new Meteor.Collection("pollchoices");
 Responses = new Meteor.Collection("responses");
