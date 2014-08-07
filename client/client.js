879a709c0aa47f33c22f1ae3ae6dec7df94d1abb
Brian Dunnette <dunn0172@umn.edu>
2014-08-07 13:33:21 -0500

    renaming responseCount -> responsePercent for clarity

--- client/client.js
+++ client/client.js
@@ -1,13 +1,13 @@
 UI.registerHelper('isOwner', function (pollId, currentUser) {
   var poll = Polls.findOne({_id:pollId});
   if (poll && currentUser) {
-    return poll.owner == currentUser._id;  
+    return poll.owner == currentUser._id;
   } else {
     return false;
   }
 });
 
-UI.registerHelper('responseCount', function (choiceId, pollId) {
+UI.registerHelper('responsePercent', function (choiceId, pollId) {
   var choiceResponses = Responses.find({
     choice: choiceId
   }).count();
